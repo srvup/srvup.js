2ee8d77002692d7c27c17c1cbdd87a22b25d90ff
Justin Mitchel <jmitchel3@gmail.com>
2018-08-18 13:58:42 -0700

    Updated Markdown for a preview toggle

--- src/markdown/Markdown.js
+++ src/markdown/Markdown.js
@@ -11,13 +11,34 @@ import {
 } from './Renders'
 
 class Markdown extends Component {
+  constructor(props){
+    super(props)
+    this.state ={
+      previewCutoff: false
+    }
+  }
+  toggleCutoff =(event)=>{
+    if (event){
+      event.preventDefault()
+    }
+    this.setState({
+      previewCutoff: !this.state.previewCutoff
+    })
+  }
+  componentDidMount () {
+    this.setState({
+      previewCutoff: this.props.previewCutoff
+    })
+  }
   render () {
     const input = this.props.input ? this.props.input : this.props.children
     const previewClass = this.props.preview === true ? 'markdown-preview' : ''
     const largePreviewClass = this.props.largePreview === true ? 'markdown-large-preview' : ''
     const minHeightClass = this.props.minHeight ? 'srvup-markdown' : ''
+    const previewCutoffClass = this.state.previewCutoff && 'markdown-preview-cutoff'
     const disallowed = []
     return (
+      <div>
       <ReactMarkdown
         className={`${this.props.className} ${previewClass} ${largePreviewClass} ${minHeightClass}`}
         source={input}
@@ -29,6 +50,15 @@ class Markdown extends Component {
           text: emojiSupport,
           shortcode: ShortCodeRenderer}}
       />
+      {this.props.previewCutoff && 
+          <button className='btn btn-link p-0' onClick={this.toggleCutoff}>
+          {this.state.previewCutoff === true ? 
+              <span>More &darr;</span> : 
+              <span>Less &uarr;</span> 
+           }
+          </button>
+       }
+      </div>
     )
   }
 }
